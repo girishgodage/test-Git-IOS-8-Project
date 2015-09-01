00d5557366962b71b30234da6b87a7135e10b605
Girish Godage <girishgodage@Girishs-Mac.local>
2015-09-01 12:56:50 +0530

    added a Test class

---
+++ GitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Girish Godage on 01/09/15.
+//  Copyright (c) 2015 Girish Godage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Girish Godage on 01/09/15.
+//  Copyright (c) 2015 Girish Godage. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
